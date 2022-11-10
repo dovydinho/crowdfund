32f55aad1be7e2827be27056fe8618093da5cae6
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-10 11:29:54 +0200

    Initial commit

--- sanity/schemas/schema.js
+++
@@ -1,9 +0,0 @@
-import createSchema from 'part:@sanity/base/schema-creator';
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-import projects from './projectSchema';
-import users from './userSchema';
-
-export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([projects, users])
-});
