77856ca2b5c5f62fb7c9f6bad6994ce41d4b5458
dovydinho <dovydas.lapinskas@tutanota.com>
2022-12-04 19:36:28 +0200

    Code clean up

--- components/web3/hooks/setupHooks.tsx
+++ components/web3/hooks/setupHooks.tsx
@@ -1,17 +1,9 @@
 import { handler as createAccountHook } from './useAccountHandler';
 import { handler as createNetworkHook } from './useNetworkHandler';
-// import { handler as createProjectsHook } from './useProjectsHandler';
 
-export const setupHooks = ({
-  provider,
-  contract
-}: {
-  provider: any;
-  contract: any;
-}) => {
+export const setupHooks = ({ provider }: { provider: any; contract: any }) => {
   return {
     useAccount: createAccountHook(provider),
     useNetwork: createNetworkHook(provider)
-    // useProjects: createProjectsHook(provider, contract)
   };
 };
